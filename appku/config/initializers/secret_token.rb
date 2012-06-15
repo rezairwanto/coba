18b4aaf47bdcf70d485c6fa17e434198878542fd
rezairwanto <reza.irwanto@wgs.co.id>
2012-06-15 11:02:48 +0700

    test

---
+++ appku/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appku::Application.config.secret_token = '3931cac75251af4e658dde408963950f7ae562d7d6901a069d7dff88f511b6d6de464a784dc57c1c60ee7a12a49d3a75efb359f5ef2d1ab77608e74a7732186b'
